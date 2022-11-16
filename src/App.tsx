82c6ec070f45fa5757aebca4f895e7224b263716
Eduardo Pinto <edfepinto@gmail.com>
2022-11-16 09:55:22 -0300

    created global styles and add assets folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { GlobalStyles } from './styles/GlobalStyles';
+
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <GlobalStyles />
+      <h1>Hello World</h1>
+    </>
   );
 }
